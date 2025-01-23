3e74398971836b33640b1fff03c424b210a5cd17
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 12:34:47 +0530

    modify app-config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideNoopAnimations(),HttpClientModule,provideHttpClient(withFetch())]
 };
