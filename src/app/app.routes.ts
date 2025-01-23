b1d15efb40e5ab04066aee3e61195dd0c5fa7404
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 20:33:18 +0530

    Implement about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReservComponent } from './reserv/reserv.component';
-import { PricingComponent } from './pricing/pricing.component'
+import { PricingComponent } from './pricing/pricing.component';
+import { AboutComponent } from './about/about.component';
 
 
 export const routes: Routes = [{ path: '', component: HomeComponent }, // Default route
     { path: 'reservations', component: ReservComponent },
-    { path: 'pricing', component: PricingComponent }];
+    { path: 'pricing', component: PricingComponent },
+    { path: 'about', component: AboutComponent }];
 
