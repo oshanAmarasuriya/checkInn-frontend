6a8fb645704b1d70f83a785a0710bbef82f23a27
oshanAmarasuriya <oshansandeep630@gmail.com>
2025-01-23 21:29:31 +0530

    Implement contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { ReservComponent } from './reserv/reserv.component';
 import { PricingComponent } from './pricing/pricing.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 export const routes: Routes = [{ path: '', component: HomeComponent }, // Default route
     { path: 'reservations', component: ReservComponent },
     { path: 'pricing', component: PricingComponent },
-    { path: 'about', component: AboutComponent }];
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent }];
 
